cea04fb4ee3a3a5814e4245091a2e5369be7dbfb
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-26 12:25:58 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,10 @@
 // declare your functions here...
 function paragraphSelector(){
-  var para = $("p")
+  var para = $("p");
   return para;
+}
+
+function lastImageSelector(){
+  var lastImg = $("img:last");
+  return lastImg
 }
